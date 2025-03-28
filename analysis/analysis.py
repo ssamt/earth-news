e53607c849b8bf23789fe838bbd3a112b9a32f30
ssamt <ssamt@kaist.ac.kr>
2025-03-28 09:31:16 +0900

    add gemini querying code

---
+++ analysis/analysis.py
@@ -0,0 +1,60 @@
+import os
+import json
+
+from dotenv import load_dotenv
+from google import genai
+
+from google_news import ArticleCollection
+
+load_dotenv()
+
+client = genai.Client(api_key=os.environ.get('GEMINI_API_KEY'))
+
+class ArticleCollectionAnalysis:
+    def __init__(self, article_collection: ArticleCollection):
+        self.article_collection = article_collection
+        self.analysis = get_analysis(article_collection)
+
+    def __str__(self):
+        return f'''{str(self.article_collection)}
+
+Location: {str(self.analysis['location'])}'''
+
+    def get_json(self):
+        return {
+            'articles': self.article_collection.get_json(),
+            'location': self.analysis['location'].get_json(),
+        }
+
+class Location:
+    def __init__(self, lat, lon):
+        self.lat = lat
+        self.lon = lon
+
+    def __str__(self):
+        return f'{self.lat}, {self.lon}'
+
+    def get_json(self):
+        return {'lat': self.lat, 'lon': self.lon}
+
+def get_json(response):
+    start_idx = response.text.find('{')
+    end_idx = response.text.rfind('}')
+    return json.loads(response.text[start_idx:end_idx+1])
+
+def get_location(article_collection: ArticleCollection):
+    query = f'''Given a list of news articles, return a location in longitude and latitude.
+Think step by step, and at the end of the response, return the location in longitude and latitude in JSON format, e.g. {{"lat": 37.7749, "lon": -122.4194}}
+
+{article_collection.get_prompt_string()}'''
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-lite',
+        contents=query,
+    )
+    answer = get_json(response)
+    return Location(answer['lat'], answer['lon'])
+
+def get_analysis(article_collection: ArticleCollection):
+    location = get_location(article_collection)
+    return {'location': location}
--- analysis/google_news.py
+++ analysis/google_news.py
@@ -11,15 +11,38 @@ class Article:
         self.title = title
         self.url = url
 
+    def __eq__(self, other):
+        return self.title == other.title
+
     def __str__(self):
         return f'{self.title}: ({self.url})'
+
+    def get_json(self):
+        return {'title': self.title, 'url': self.url}
 
 class ArticleCollection:
     def __init__(self, articles):
         self.articles = articles
 
+    # Mainly for caching purposes
+    # Returns whether two collections overlap, instead of whether they are equal
+    # I know it's not transitive, come up with a better way if you can.
+    def __eq__(self, other):
+        intersection = set(self.articles) & set(other.articles)
+        has_intersection = len(intersection) > 0
+        return has_intersection
+
     def __str__(self):
         return '\n'.join(str(article) for article in self.articles)
+
+    def get_json(self):
+        return [article.get_json() for article in self.articles]
+
+    def get_prompt_string(self):
+        return '\n'.join([
+            f'{i + 1}. {article.title}'
+            for i, article in enumerate(self.articles)
+        ])
 
 def get_sections_urls(sections):
     page = requests.get(base_url)
